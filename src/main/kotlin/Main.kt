c0debc32e37441a8fb7a9397c0fb052a5d9af7db
PiunNickolay <147736400+PiunNickolay@users.noreply.github.com>
2024-06-02 16:44:44 +0300

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,12 @@
 package ru.netology
 
 fun main() {
-    println("Hello World!")
+    val payment = 10_001
+    val isRegularCustomer = true
+    var priceWithDiscount: Int = if((payment>=1001)&&(payment<10_001)) payment - 100
+    else if(payment>=10_001) payment-(payment*5/100)
+    else payment
+    var priceWithRegular: Int = if(isRegularCustomer === true) priceWithDiscount-(priceWithDiscount*1/100)
+    else priceWithDiscount
+    println("Итоговая сумма покупки, включая скидки: $priceWithRegular")
 }
